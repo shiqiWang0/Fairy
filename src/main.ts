7c5f109e4d1e8bbc36c3b5d7f314e5b937eb52ee
beier <beier@dtstack.com>
2022-05-25 15:57:18 +0800

    feat: add ignore nodeModules

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+const program = require('commander');
+const { version } = require('./utils/constants.ts');
+const { mapActions } = require('./utils/common.ts');
+
+Reflect.ownKeys(mapActions).forEach((action) => {
+       program.command(action) //配置命令的名字
+              .alias(mapActions[action].alias) // 命令的别名
+              .description(mapActions[action].description) // 命令对应的描述
+              .action(() => {  //动作
+                     if (action === '*') {  //访问不到对应的命令 就打印找不到命令
+                            console.log(mapActions[action].description);
+                     } else {
+                            console.log(action);
+                            // 分解命令 到文件里 有多少文件 就有多少配置 create config
+                            // fairy-cli create project-name ->[node,fairy-cli,create,project-name]
+                            console.log(process.argv);
+                     }
+              })
+});
+
+// 监听用户的help事件
+program.on('--help', () => {
+       console.log('\nExamples:');
+       Reflect.ownKeys(mapActions).forEach((action) => {
+              mapActions[action].examples.forEach((example) => {
+                     console.log(` ${example}`);
+              })
+       })
+})
+
+
+program.version(version)
+       .parse(process.argv); // process.argv就是用户在命令行中传入的参数
